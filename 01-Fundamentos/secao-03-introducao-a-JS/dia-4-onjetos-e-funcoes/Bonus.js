0823e464ff233668f3e7f935320512789a5d9514
Francisco Motta <motta.lopes@hotmail.com>
2023-01-25 02:05:21 -0300

    last exercise

--- 01-Fundamentos/secao-03-introducao-a-JS/dia-4-onjetos-e-funcoes/Bonus.js
+++ 01-Fundamentos/secao-03-introducao-a-JS/dia-4-onjetos-e-funcoes/Bonus.js
@@ -42,24 +42,45 @@
 
 // console.log(numerals(teste));
 
-let vector = [[1, 2], [3, 4, 5, 6], [7, 8, 9, 10]];
+// let vector = [[1, 2], [3, 4, 5, 6], [7, 8, 9, 10]];
 
-function arrayOfNumbers(vector) {
-    let newArray = [];
-    let evenArray = [];
+// function arrayOfNumbers(vector) {
+//     let newArray = [];
+//     let evenArray = [];
 
-    for (let key of vector) {
-        newArray = key;
+//     for (let key of vector) {
+//         newArray = key;
 
-        for (let positon of newArray) {
-            
-            if(positon % 2 === 0){
-                evenArray.push(positon);
-            }
-        }
-    }
+//         for (let positon of newArray) {
 
-    return evenArray
-}
- 
-console.log(arrayOfNumbers(vector));
+//             if(positon % 2 === 0){
+//                 evenArray.push(positon);
+//             }
+//         }
+//     }
+
+//     return evenArray
+// }
+
+// console.log(arrayOfNumbers(vector));
+
+
+const basket = [
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',
+];
