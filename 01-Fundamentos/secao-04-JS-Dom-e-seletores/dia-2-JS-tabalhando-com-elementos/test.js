5d90e4e1aad3a14cf9649db98ab0adeacbd5ee86
Francisco Motta <motta.lopes@hotmail.com>
2023-02-12 15:47:36 -0300

    commitando para resetar exercicios

--- 01-Fundamentos/secao-04-JS-Dom-e-seletores/dia-2-JS-tabalhando-com-elementos/test.js
+++ 01-Fundamentos/secao-04-JS-Dom-e-seletores/dia-2-JS-tabalhando-com-elementos/test.js
@@ -1,47 +1,50 @@
-let x = document.getElementById('elementoOndeVoceEsta');
-x.parentElement.style.color = 'red'
+// let x = document.getElementById('elementoOndeVoceEsta');
+// x.parentElement.style.color = 'red'
 
-let y = document.getElementById('primeiroFilhoDoFilho')
-y.innerText = 'Adicionar um texto a ele'
+// let y = document.getElementById('primeiroFilhoDoFilho')
+// y.innerText = 'Adicionar um texto a ele'
 
-document.getElementById('pai').firstElementChild
+// document.getElementById('pai').firstElementChild
 
-document.getElementById('elementoOndeVoceEsta').firstElementChild
+// document.getElementById('elementoOndeVoceEsta').firstElementChild
 
-document.getElementById('elementoOndeVoceEsta').previousElementSibling
+// document.getElementById('elementoOndeVoceEsta').previousElementSibling
 
-document.getElementById('elementoOndeVoceEsta').nextSibling
+// document.getElementById('elementoOndeVoceEsta').nextSibling
 
-document.getElementById('elementoOndeVoceEsta').nextElementSibling
+// document.getElementById('elementoOndeVoceEsta').nextElementSibling
 
-document.getElementById('pai').lastElementChild.previousElementSibling
+// document.getElementById('pai').lastElementChild.previousElementSibling
 
-// Crie um irmão para `elementoOndeVoceEsta`.
+// // Crie um irmão para `elementoOndeVoceEsta`.
 
-const pai = document.getElementById('pai'); // Recupere o elemento com o id pai
+// const pai = document.getElementById('pai'); // Recupere o elemento com o id pai
 
-const irmaoElementoOndeVoceEsta = document.createElement('section'); // Crie um novo elemento
+// const irmaoElementoOndeVoceEsta = document.createElement('section'); // Crie um novo elemento
 
-irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
+// irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
 
-pai.appendChild(irmaoElementoOndeVoceEsta); // Adicione o elemento criado como filho do elemento com o id `pai`
-
-
-// crie um filho para elementoOndeVoceEsta
-let a = document.getElementById('elementoOndeVoceEsta');
-
-let b = document.createElement('section');
-b.id = 'SectionFilho'
-
-a.appendChild(b);
-
-let primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho')
-let filhoDoFilhoDoFilho = document.createElement('section')
-
-filhoDoFilhoDoFilho.id = 'filhoDoFilhoDoFilho'
-
-primeiroFilhoDoFilho.appendChild(filhoDoFilhoDoFilho);
+// pai.appendChild(irmaoElementoOndeVoceEsta); // Adicione o elemento criado como filho do elemento com o id `pai`
 
 
-let essequi = document.getElementById('filhoDoFilhoDoFilho')
-essequi.parentElement.parentElement.nextElementSibling;
+// // crie um filho para elementoOndeVoceEsta
+// let a = document.getElementById('elementoOndeVoceEsta');
+
+// let b = document.createElement('section');
+// b.id = 'SectionFilho'
+
+// a.appendChild(b);
+
+// let primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho')
+// let filhoDoFilhoDoFilho = document.createElement('section')
+
+// filhoDoFilhoDoFilho.id = 'filhoDoFilhoDoFilho'
+
+// primeiroFilhoDoFilho.appendChild(filhoDoFilhoDoFilho);
+
+
+// let essequi = document.getElementById('filhoDoFilhoDoFilho')
+// essequi.parentElement.parentElement.nextElementSibling;
+
+// let a = document.getElementById("elementoOndeVoceEsta")
+// a.removeChild(a.firstElementChild);
